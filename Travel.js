c302485f7da2d036c1d240444bd7234f102b2ef6
Nethmi25 <166733661+nethmi1225@users.noreply.github.com>
2024-12-01 12:19:15 +0530

    Create Travel.js

---
+++ Travel.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.book-table-img-slider', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
